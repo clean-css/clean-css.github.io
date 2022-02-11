31d38ca44a9db16c3c9f7f573767f37f60ee9db4
johannchopin <johannchopin@pm.me>
2022-02-11 15:04:07 +0100

    ⚰️ Remove unused css

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -5,9 +5,3 @@
 <footer class="text-center mt-5 mb-2">
   Hosted with <Emoji symbol="❤️ " label="love" /> on <a href="//pages.github.com/">GitHub Pages</a>.
 </footer>
-
-<style>
-  span {
-    color: hsl(0, 100%, 45%);
-  }
-</style>
