e0124f81289fc9158f08ede60b9eac527ac62412
johannchopin <johannchopin@pm.me>
2022-02-11 15:03:41 +0100

    🚚 Fix link to issues

--- src/components/Legend.svelte
+++ src/components/Legend.svelte
@@ -1,4 +1,8 @@
+<script>
+  import { GITHUB_REPO_URL } from '../constants'
+</script>
+
 <p class="mt-5">
   CSS optimizing happens directly in your browser using a browser-compatible build of clean-css. If you miss any functionality, you are more than welcome to 
-  <a href="https://github.com/clean-css/clean-css.github.io" target="_blank">add it</a>!
+  <a href={`${GITHUB_REPO_URL}/issues`} target="_blank">add it</a>!
 </p>
---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const GITHUB_REPO_USER = 'clean-css'
+export const GITHUB_REPO_NAME = 'clean-css'
+export const GITHUB_REPO_URL = `https://github.com/${GITHUB_REPO_USER}/${GITHUB_REPO_NAME}`
